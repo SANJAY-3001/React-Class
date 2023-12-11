560620c43cbb2bf88d35f51399902471217a6bed
SANJAY-3001 <sanjayp7639@gmail.com>
2023-12-11 16:24:54 +0530

    Custom component car

--- Day3/sanjay-search-engine/src/App.js
+++ Day3/sanjay-search-engine/src/App.js
@@ -1,13 +1,14 @@
 import ClassComponent from './Component/ClassComponent';
 import Greeting from './Component/Greeting';
 import FunctionComponent from './Component/funtioncomponent';
-
+import ClassCar from './Component/ClassCar';
 function App() {
   return (
     <div>
       <FunctionComponent />
       <ClassComponent />
       <Greeting></Greeting>
+      <ClassCar />
     </div>
   );
 }
